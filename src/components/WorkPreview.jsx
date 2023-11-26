8c367c3dd984c57bb4afbcdb7c0689cf229196c8
trcast <59939928+trcast@users.noreply.github.com>
2023-11-26 17:38:54 -0500

    added work hover image

--- src/components/WorkPreview.jsx
+++ src/components/WorkPreview.jsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 
 const WorkPreview = ({ selectedService }) => {
   const [works, setWorks] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [hoveredWork, setHoveredWork] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -39,30 +42,74 @@ const WorkPreview = ({ selectedService }) => {
         .sort((a, b) => a.acf.order - b.acf.order)
     : works.sort((a, b) => a.acf.order - b.acf.order);
 
+  const handleMouseEnter = (work) => {
+    setHoveredWork(work);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredWork(null);
+  };
+
   return (
     <div className="work-preview-container">
-      {filteredWorks.map((work) => (
-        <div key={work.id} className="work-preview-wrapper">
-          <Link to={`/work/${work.id}`} className="work-preview-link">
-            <div className="work-preview-info hover-right-position">
-              <div className="work-preview-name-client">
-                <h5>{work.acf.project_name}</h5>
-                <p className="gray work-preview-client">{work.acf.client}</p>
+      {filteredWorks.map((work) => {
+        return (
+          <div
+            key={work.id}
+            className="work-preview-wrapper"
+            onMouseMove={(e) =>
+              setMousePosition({
+                x: e.clientX,
+                y: e.clientY + window.scrollY,
+              })
+            }
+            onMouseEnter={() => {
+              setIsHovered(true);
+              handleMouseEnter(work);
+            }}
+            onMouseLeave={() => {
+              setIsHovered(false);
+              handleMouseLeave();
+            }}
+          >
+            <Link to={`/work/${work.id}`} className="work-preview-link">
+              <div className="work-preview-info hover-right-position">
+                <div className="work-preview-name-client">
+                  <h5>{work.acf.project_name}</h5>
+                  <p className="gray work-preview-client">{work.acf.client}</p>
+                </div>
+                <div className="work-preview-services-container">
+                  {work.acf.services.map((service) => (
+                    <p className="gray" key={service.term_id}>
+                      {service.name}
+                    </p>
+                  ))}
+                </div>
+                <div className="work-preview-arrow">
+                  <img src={arrowSm} alt="" />
+                </div>
               </div>
-              <div className="work-preview-services-container">
-                {work.acf.services.map((service) => (
-                  <p className="gray" key={service.term_id}>
-                    {service.name}
-                  </p>
-                ))}
-              </div>
-              <div className="work-preview-arrow">
-                <img src={arrowSm} alt="" />
-              </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
+        );
+      })}
+      {isHovered && hoveredWork && (
+        <div
+          className="project-image-container"
+          style={{
+            top: `${mousePosition.y - 5}px`,
+            left: `${mousePosition.x + 5}px`,
+            position: "absolute",
+            zIndex: 1000,
+          }}
+        >
+          <img
+            src={hoveredWork.acf.main_project_image}
+            alt="Main Project"
+            style={{ maxWidth: "350px" }}
+          />
         </div>
-      ))}
+      )}
     </div>
   );
 };
