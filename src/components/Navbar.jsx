c900932e5a2600042051bad9a741e72447082218
trcast <59939928+trcast@users.noreply.github.com>
2023-11-27 14:35:44 -0500

    updated nav bar style, and responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,48 @@
 import React from "react";
 import drLogo from "../assets/client/dr-logo.svg";
 import "../App.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav>
       <Link to="/" className="link-style">
         <img src={drLogo} alt="" />
       </Link>
       <div className="nav-links-container p-semibold">
-        <Link to="/work">
-          <p className="link-style hover-underline-animation">Work</p>
+        <Link
+          to="/work"
+          className={
+            location.pathname === "/work" ? "active-link" : "link-style"
+          }
+        >
+          <p className="hover-underline-animation">Work</p>
         </Link>
-        <Link to="/about">
-          <p className="link-style hover-underline-animation">About</p>
+        <Link
+          to="/about"
+          className={
+            location.pathname === "/about" ? "active-link" : "link-style"
+          }
+        >
+          <p className="hover-underline-animation">About</p>
         </Link>
-        <Link to="/contact">
-          <p className="link-style hover-underline-animation">Contact</p>
+        <Link
+          to="/contact"
+          className={
+            location.pathname === "/contact" ? "active-link" : "link-style"
+          }
+        >
+          <p className="hover-underline-animation">Contact</p>
         </Link>
-        <Link to="/jobs">
-          <p className="link-style hover-underline-animation">Jobs</p>
+        <Link
+          to="/jobs"
+          className={
+            location.pathname === "/jobs" ? "active-link" : "link-style"
+          }
+        >
+          <p className="hover-underline-animation">Jobs</p>
         </Link>
       </div>
     </nav>
